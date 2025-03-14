e9c3026593420e00a163abca1202415824281f78
Disitha <disithar@gmail.com>
2025-03-14 20:42:24 +0530

    Enhance index screen with improved welcome message and additional descriptive text

--- CourseMate/app/index.tsx
+++ CourseMate/app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View, Image } from "react-native";
 import Colors from "../constants/Colors";
+import { StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View
@@ -21,8 +22,25 @@ export default function Index() {
         borderTopLeftRadius:35,
         borderTopRightRadius:35,
       }}>
-        <Text>Welcome to CourseMate</Text>
+        <Text style={{
+          fontSize: 30,
+          fontWeight: "bold",
+          textAlign: "center",
+          color: Colors.WHITE,
+        }}>Welcome to CourseMate</Text>
+        <Text style={{
+          fontSize: 20,
+          color: Colors.WHITE,
+          textAlign: "center",
+          marginTop: 20,
+        }}>Unlock endles learning opportunities and shape your future with the perfect course. Whether you're exploring new fields, advancing your career, or starting fresh, CourseMate is here to guide you every step of the way!</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button:{
+    
+  }
+})
