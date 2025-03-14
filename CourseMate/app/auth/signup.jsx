4282296e38b728721eda981c972109de81aaad1f
Disitha <disithar@gmail.com>
2025-03-15 00:46:11 +0530

    Refactor Signup screen: add input fields for fullname, email, and password; implement account creation button; enhance navigation to SignIn screen

--- CourseMate/app/auth/signup.jsx
+++ CourseMate/app/auth/signup.jsx
@@ -1,13 +1,16 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity, Pressable } from "react-native";
 import Colors from "../../constants/Colors";
-//import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 export default function Signup(){
+    const router=useRouter();
     return(
         <View style={{
             display:'flex',
             alignItems:'center',
             paddingTop: 10,
             flex: 1,
+            padding: 25,
             backgroundColor:Colors.WHITE,
         }}>
             <Image source={require('../../assets/images/logo.png')}
@@ -20,6 +23,55 @@ export default function Signup(){
                 fontSize: 30,
                 fontFamily: "outfit-bold",
             }}>Create New Account</Text>
+
+            <TextInput placeholder='Fullname' style={styles.TextInput}/>
+            <TextInput placeholder='Email' style={styles.TextInput}/>
+            <TextInput placeholder='Password' secureTextEntry={true} style={styles.TextInput}/>
+
+            <TouchableOpacity style={{
+                backgroundColor: Colors.PRIMARY,
+                padding: 15,
+                width: '100%',
+                borderRadius: 10,
+                marginTop: 25,
+            }}>
+                <Text style={{
+                    fontFamily: "outfit",
+                    fontSize:20,
+                    color: Colors.WHITE,
+                    textAlign: 'center',
+                }}>Create Account</Text>
+            </TouchableOpacity>
+
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row', gap:5,
+                    marginTop: 20,
+                }}>
+                     <Text style={{
+                        fontFamily: "outfit",
+                     }}>Already have an account?</Text>
+                    <Pressable
+                        onPress={()=>router.push('/auth/signin')}
+                    >
+                            <Text style={{
+                                color: Colors.PRIMARY,
+                                fontFamily: "outfit-bold",
+                            }}>Sgin-In Here</Text>
+                    </Pressable>
+                </View>
+           
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    TextInput:{
+        borderWidth: 1,
+        width: '100%',
+        padding: 15,
+        fontSize: 18,
+        marginTop: 20,
+        borderRadius: 10,
+    }
+})
