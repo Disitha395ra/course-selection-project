93628bca2987f7691c2ea5b5ac7ae8857cc34ac2
Disitha <disithar@gmail.com>
2025-03-14 20:21:24 +0530

    Enhance index screen layout with welcome message and adjust image height

--- CourseMate/app/index.tsx
+++ CourseMate/app/index.tsx
@@ -11,8 +11,18 @@ export default function Index() {
       <Image source={require("../assets/images/landcover.jpg")}
       style={{
         width: "100%",
-        height: 300,
+        height: 350,
+        marginTop:100,
       }}/>
+      <View style={{
+        padding:20,
+        backgroundColor:Colors.PRIMARY,
+        height:'100%',
+        borderTopLeftRadius:35,
+        borderTopRightRadius:35,
+      }}>
+        <Text>Welcome to CourseMate</Text>
+      </View>
     </View>
   );
 }
