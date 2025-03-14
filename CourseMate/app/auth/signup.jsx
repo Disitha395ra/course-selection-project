777c4afc7b60c49e559361105c2b37ceead07e61
Disitha <disithar@gmail.com>
2025-03-15 00:19:11 +0530

    Refactor Signup screen: update layout with improved styling, add logo image, and enhance text presentation

--- CourseMate/app/auth/signup.jsx
+++ CourseMate/app/auth/signup.jsx
@@ -1,10 +1,25 @@
 import { Text, View, Image } from "react-native";
-//import Colors from "../constants/Colors";
+import Colors from "../../constants/Colors";
 //import { StyleSheet } from "react-native";
 export default function Signup(){
     return(
-        <View>
-            <Text>SignUp</Text>
+        <View style={{
+            display:'flex',
+            alignItems:'center',
+            paddingTop: 10,
+            flex: 1,
+            backgroundColor:Colors.WHITE,
+        }}>
+            <Image source={require('../../assets/images/logo.png')}
+            style={{
+                width: 380,
+                height: 300,
+            }}
+            />
+            <Text style={{
+                fontSize: 30,
+                fontFamily: "outfit-bold",
+            }}>Create New Account</Text>
         </View>
     )
 }
