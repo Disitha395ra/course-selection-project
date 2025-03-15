c588ae089c558f850e294b19fcc314ea3444d786
Disitha <disithar@gmail.com>
2025-03-15 11:36:25 +0530

    Add UserDetailContext for managing user state in RootLayout

--- CourseMate/app/_layout.tsx
+++ CourseMate/app/_layout.tsx
@@ -1,16 +1,23 @@
 import { Stack } from "expo-router";
 import {useFonts} from 'expo-font'
+import {UserDetailContext} from "../context/UserDetailContext";
+import {useState} from 'react';
 export default function RootLayout() {
   useFonts({
     'outfit':require('../assets/fonts/Outfit-Regular.ttf'),
     'outfit-bold':require('../assets/fonts/Outfit-Bold.ttf'),
     'outfit-medium':require('../assets/fonts/Outfit-Medium.ttf'),
   })
+
+  const [userDetail, setUserDetail] = useState();
+
   return (
+    <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
     <Stack screenOptions={{ headerShown: false 
 
     }}>
 
     </Stack>
+    </UserDetailContext.Provider>
   )
 }
