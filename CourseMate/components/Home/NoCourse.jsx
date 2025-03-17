8b97d07f57c5db1ac994fef6a35d39e982d0ea39
SanduniAmasha <139688263+SanduniAmasha@users.noreply.github.com>
2025-03-17 21:23:44 +0530

    Add image and text along with styles into nocourse file

--- CourseMate/components/Home/NoCourse.jsx
+++ CourseMate/components/Home/NoCourse.jsx
@@ -4,8 +4,20 @@ export default function NoCourse(){
     return(
         <View style={{
             marginTop:40,
+            display:'flex',
+            alignItems: 'center',
         }}>
-            <Image source={require('../../assets/images/nocourse.png')}/>
+            <Image source={require('../../assets/images/nocourse.png')}
+                style={{
+                    height: 200,
+                    width: 200,
+                }}
+            />
+            <Text style={{
+                fontFamily: 'outfit-bold',
+                fontSize: 25,
+                textAlign:'center',
+            }}>You Don't Have Any Courses</Text>
         </View>
     )
 }
